d1e7d754482ffab8c876f8a6405f00be616f0a15
Vit Onischenko <slonik0775@gmail.com>
2022-07-22 12:44:40 +0300

    button path#1

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,43 @@
-import React from 'react';
+import React, {MouseEvent} from 'react';
 import './App.css';
-import {Header} from './site/Header';
-import {Body} from './site/Body';
-import {Footer} from './site/Footer';
-import {CarTable, NewComponent} from './NewComponent';
-
-
-
-const students = [
-  {id: 1, name: "James", age: 8},
-  {id: 2, name: "Robert", age: 18},
-  {id: 3, name: "John", age: 28},
-  {id: 4, name: "Michael", age: 38},
-  {id: 5, name: "William", age: 48},
-  {id: 6, name: "David", age: 58},
-  {id: 7, name: "Richard", age: 68},
-  {id: 8, name: "Joseph", age: 78},
-  {id: 9, name: "Thomas", age: 88},
-  {id: 10, name: "Charles", age: 98},
-  {id: 11, name: "Christopher", age: 100},
-]
-
-const topCars = [
-  {manufacturer:"BMW",model:'m5cs'},
-  {manufacturer:"Mercedes",model:'e63s'},
-  {manufacturer:"Audi",model:'rs6'}
-]
 
 
 function App() {
-  return (
 
-<div>
-  <Header title={'NEW Header'} />
-  <Body titleForBody={'NEW BODY'} />
-  <Footer titleForFooter={'NEW Footer'} />
-  <NewComponent students={students} />
-  <CarTable topCars={topCars} />
-</div>  );
+    // const myFirstSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Hello, i am Vit');
+    // };
+    //
+    //
+    // const myTwoSubscriber = (event: MouseEvent<HTMLButtonElement>) => {
+    //     console.log('Kis Kis');
+    // };
+
+    const onClickHandler = (name: string) => {
+           console.log(name);
+
+    }
+
+
+
+    // const foo1 = () => {
+    //     console.log('100200');
+    // }
+    // const foo2 = (num: number) => {
+    //     console.log(`num: ${num}`)
+    // }
+
+    return (
+
+        <div className="App">
+            {/*<button onClick={(event) => {console.log('Hello');}}>MyYoutubeChanel-1</button>*/}
+            <button onClick={() => {onClickHandler('Vit')}}>MyYoutubeChanel-1</button>
+            <button name={'vit'} onClick={() => {onClickHandler('Misha')}}> MyYoutubeChanel-2</button>
+
+            {/*<button onClick={foo1}>1</button>*/}
+            {/*<button onClick={() => {foo2(100200) }}>2</button>*/}
+        </div>);
+
 }
 
 export default App;
