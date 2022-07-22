29f537c4edbaa0a746a7bb27233461d5cb44e500
Vit Onischenko <slonik0775@gmail.com>
2022-07-22 18:05:08 +0300

    filter path2

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,43 @@
-import React, {MouseEvent, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import Button from './components/Button';
 
 
 function App() {
 
-
-    let [a, setA] = useState(1,);
-
-    function onclickHandler() {
-        setA(++a);
-    }
-
-    let [b] = useState(0,);
-
-
-    function resetHandler() {
-        setA(a =b)
-    }
+    const [money, setMoney] = useState([
+        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
+        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
+        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
+        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
+        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
+    ]);
+    let currentMoney = money;
 
     return (
         <div>
 
-            <h1>
-                <div>{a}
-                    <button onClick={onclickHandler}>number</button>
-                </div>
 
-                <div> {b}
-                    <button onClick={resetHandler}>number</button>
-                </div>
-            </h1>
+            <ul>
+                {currentMoney.map((objFromMoneyArr, index) => {
+                    return (
+                        <li key={index}>
+                            <span> {objFromMoneyArr.banknote}</span>
+                            <span> {objFromMoneyArr.nominal}</span>
+                            <span> {objFromMoneyArr.number}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+            {/*<div style={{marginLeft: '35px'}}>*/}
+            {/*    <button onClick={() => onClickFilterHandler('all')}>all</button>*/}
+            {/*    <button onClick={() => onClickFilterHandler('ruble')}>rubles</button>*/}
+            {/*    <button onClick={() => onClickFilterHandler("dollar")}>dollars</button>*/}
+            {/*</div>*/}
         </div>);
-
 }
 
 export default App;
