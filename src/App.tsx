4f64555e9aee5fa94f581e68ef0bd35976c512da
Vit Onischenko <slonik0775@gmail.com>
2022-07-26 13:23:06 +0300

    input start lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
 import React, {useState} from 'react';
 import './App.css';
-import NewComponent from './components/NewComponent';
-import Button from './components/Button';
+
 
 //вернул коммит
 function App() {
+    const [message, setMessage] = useState([
+            {message: 'message1'},
+            {message: 'message2'},
+            {message: 'message3'},
+            {message: 'message4'},
+            {message: 'message5'}
+        ]
+    );
 
-    const [money, setMoney] = useState([
-        {banknote: 'dollar', nominal: 100, number: ' a1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' z1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' w1234567890'},
-        {banknote: 'dollar', nominal: 100, number: ' e1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' c1234567890'},
-        {banknote: 'ruble', nominal: 100, number: ' r1234567890'},
-        {banknote: 'dollar', nominal: 50, number: ' x1234567890'},
-        {banknote: 'ruble', nominal: 50, number: ' v1234567890'},
-    ]);
-    let currentMoney = money;
-    const [filter, setFilter] = useState('all');
-
-    if (filter === 'dollar') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknote === 'dollar');
-    }
-    if (filter === 'ruble') {
-        currentMoney = money.filter(filteredMoney => filteredMoney.banknote === 'ruble');
-    }
-
-    function onClickFilterAppHandler(nameButton: string) {
-        setFilter(nameButton);
-    }
 
     return (
-        <div>
+        <div className={'App'}>
+            <div>
+                <input/>
+                <button> +</button>
 
-            <NewComponent m={currentMoney}/>
-            <div style={{marginLeft: '35px', display: 'flex'}}>
-                <Button name={'all'} callBack={() => onClickFilterAppHandler('all')}/>
-                <Button name={'dollar'}callBack={() => onClickFilterAppHandler('dollar')}/>
-                <Button name={'ruble'} callBack={() => onClickFilterAppHandler('ruble')}/>
+            </div>
+
+            {message.map((el, index) => {
+                return (
+                    <div key={index}> {el.message}</div>);
+            })}
+
         </div>
-</div>);
+
+    );
 }
 
 export default App;
